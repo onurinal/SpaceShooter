40e8a0166f273e74ff0f4d254bfc1457b62aa236
Onurinal <onurinaldev@gmail.com>
2024-10-25 03:45:42 +0300

    + Added player ship + Added player movements with keybord and touch

---
+++ Assets/SpaceShooter/Scripts/Player/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+
+namespace SpaceShooter.Player
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField] private PlayerProperties playerProperties;
+        [SerializeField] private Rigidbody2D myRigidbody2D;
+
+        private Camera mainCamera;
+
+        private void Start()
+        {
+            mainCamera = Camera.main;
+        }
+
+        private void FixedUpdate()
+        {
+            // MoveShipWithKeyboard();
+            MoveShipWithTouch();
+        }
+
+        private void MoveShipWithTouch()
+        {
+            var touchPosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = new Vector3(touchPosition.x, touchPosition.y + 0.3f, transform.position.z);
+        }
+
+        private void MoveShipWithKeyboard()
+        {
+            var direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+            if (direction != Vector2.zero)
+            {
+                myRigidbody2D.velocity = new Vector2(direction.x * playerProperties.playerMoveSpeed,
+                    direction.y * playerProperties.playerMoveSpeed);
+            }
+            else
+            {
+                myRigidbody2D.velocity = Vector2.zero;
+            }
+        }
+    }
+}
---
+++ Assets/SpaceShooter/Scripts/Player/PlayerProperties.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace SpaceShooter.Player
+{
+    [CreateAssetMenu(fileName = "PlayerProperties", menuName = "SpaceShooter/Player/Create New PlayerProperties")]
+    public class PlayerProperties : ScriptableObject
+    {
+        public float playerMoveSpeed;
+    }
+}
