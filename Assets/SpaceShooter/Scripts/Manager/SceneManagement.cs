665a639ed9e6cb9386d76de8cba1e23bed632197
Onurinal <onurinaldev@gmail.com>
2024-11-07 19:37:35 +0300

    + Added basic scene management

---
+++ Assets/SpaceShooter/Scripts/Manager/SceneManagement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace SpaceShooter.Manager
+{
+    public class SceneManagement : MonoBehaviour
+    {
+        public static SceneManagement Instance;
+
+        private void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                Instance = this;
+            }
+        }
+
+        public void LoadSameScene()
+        {
+            var currentScene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(currentScene.buildIndex);
+        }
+    }
+}
